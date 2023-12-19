7f5566f7640611bdf731155a482fe3a7a43f4410
Maho Pacheco <mapach@microsoft.com>
2023-12-19 13:57:50 -0800

    Updating to SK 1.0.1

---
+++ api/src/SK/IPromptsFactory.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+using Microsoft.SemanticKernel;
+
+namespace AzDoCopilotSK.SK
+{
+    public interface IPromptsFactory
+    {
+        KernelFunction GetTestCasePrompt();
+        KernelFunction GetUserStoryPrompt(string userStoryStyle);
+    }
+}
